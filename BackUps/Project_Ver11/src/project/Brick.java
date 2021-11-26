e9c92ac1767332fcc9c2be5ccbfe759e82e09298
NicholasBaxley <47706880+NicholasBaxley@users.noreply.github.com>
2021-11-25 20:42:49 -0500

    Add files via upload

---
+++ BackUps/Project_Ver11/src/project/Brick.java
@@ -0,0 +1,76 @@
+package project;
+
+// This class hold all of the infomation about each individual brick
+public class Brick {
+    String name;
+    String type;  
+    double length;
+    double height;
+    double width;
+    
+    double costPerBrick;
+    
+    public Brick()
+    {
+        name = "";
+        type = "";
+        length = 0;
+        height = 0;
+        width = 0;
+    }
+    // Need to add Width in due to my stupidity
+    public Brick(String Name, String Type, double Length, double Height, double CostPerBrick)
+    {
+        name = Name;
+        type = Type;
+        length = Length;
+        height = Height;
+        costPerBrick = CostPerBrick;        
+    }
+    
+    public Brick(String Name, String Type, double Length, double Height, double Width, double CostPerBrick)
+    {
+        name = Name;
+        type = Type;
+        length = Length;
+        height = Height;
+        width = Width;
+        costPerBrick = CostPerBrick;        
+    }
+    
+    public double getLength()
+    {
+        return length;
+    }
+
+    public double getHeight()
+    {
+        return height;
+    }
+    public double getWidth()
+    {
+        return width;
+    }
+    public double getCostPerBrick()
+    {
+        return costPerBrick;
+    }
+    
+    public String getType()
+    {
+        return type;
+    }
+    
+    public double getPalletCost(int pallets)
+    {
+        return (costPerBrick * 100) * pallets;
+    }
+    
+    public String getInfo()
+    {
+        return String.format("\n%-40s%8s%11sin. x %8sin.$%.2f",name,type,length,height,costPerBrick);
+    }
+}   
+
+
+// "\n%-40s%-8s%11sin. x %8sin.$%.2f",name,type,length,height,costPerBrick
